a0ac87e9aba1a5aeb123b6406711b6b3f611b286
timore333 <timore333@gmail.com>
2020-11-30 13:24:08 +0200

    installed matice localization

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,7 +7,27 @@ import Vue from 'vue';
 import { InertiaApp } from '@inertiajs/inertia-vue';
 import { InertiaForm } from 'laravel-jetstream';
 import PortalVue from 'portal-vue';
+import {__, trans, setLocale, getLocale, transChoice, MaticeLocalizationConfig, locales} from "matice"
 
+Vue.mixin({
+    methods: {
+        $trans: trans,
+        $__: __,
+        $transChoice: transChoice,
+        $setLocale: (locale) => {
+          setLocale(locale);
+          this.$forceUpdate() // Refresh the vue instance after locale change.
+        },
+        // The current locale
+        $locale() {
+            return getLocale()
+        },
+        // A listing of the available locales
+        $locales() {
+            return locales()
+        }
+    },
+});
 Vue.mixin({ methods: { route } });
 Vue.use(InertiaApp);
 Vue.use(InertiaForm);
