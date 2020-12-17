9af5b490cf4cafdcaf0d203959de8097478ec9dd
timore333 <timore333@gmail.com>
2020-12-17 19:30:40 +0200

    created setting seeder

---
+++ database/seeders/SettingSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Tenants\Setting\Setting;
+use Illuminate\Database\Seeder;
+
+class SettingSeeder extends Seeder
+{
+
+    public $setting = [
+        ['name'=>'skin','value' => 'skin-default-dark'],
+        ['name'=>'theme','value' => 'dark'],
+        ['name'=>'locale','value' => 'en'],
+    ];
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        foreach($this->setting as $setting){
+            Setting::create($setting);
+        }
+    }
+}
